68864c053a47f57a5fe077f08a01f6dc8e103e73
SureshKaushik <suresh.kau05@gmail.com>
2018-01-26 07:00:20 +0530

    Initial version

---
+++ MyGitRepo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGitRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayExample();
+            Console.ReadLine();
+        }
+
+        private static void ArrayExample()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
